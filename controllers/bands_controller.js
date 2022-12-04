e4e1d75d2954123d47f11a34aa73f1e9e55473a0
Mary Elizabeth <maryelizabeth@Marys-MacBook-Pro.local>
2022-12-03 23:47:58 -0600

    added controllers

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -16,3 +16,31 @@ bands.get('/', async (req, res) => {
     }
 })
 
+// FIND A SPECIFIC BAND
+bands.get('/:id', async (req, res) => {
+    try {
+        const foundBand = await Band.findOne({
+            where: { band_id: req.params.id }
+        })
+        res.status(200).json(foundBand)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+// CREATE A BAND
+bands.post('/', async (req, res) => {
+    try {
+        const newBand = await Band.create(req.body)
+        res.status(200).json({
+            message: 'Successfully inserted a new band',
+            data: newBand
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+
+
+
